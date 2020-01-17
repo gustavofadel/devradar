9bc494b00819a1f61ca0cf98f69be8d5536c81b2
Gustavo Fadel <gustavohenriquefrancofadel@gmail.com>
2020-01-17 11:09:06 -0400

    Added WebSocket connection

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const { findConnections, sendMessage } = require('../websocket')
 
 module.exports = {
   async index (request, response) {
@@ -34,6 +35,13 @@ module.exports = {
         techs: techsArray,
         location
       })
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev)
     }
 
     return response.json(dev)
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
+
 const routes = require('./routes')
+const { setWebsocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setWebsocket(server)
 
 mongoose.connect('mongodb+srv://gustavofadel:eshabib@cluster0-9em9v.mongodb.net/devradar?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -14,4 +20,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
